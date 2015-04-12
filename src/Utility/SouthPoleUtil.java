4aa647bafcd29e865c5744c78fe592d41a3c61fc
William Liddy <wjl36@case.edu>
2015-04-12 02:41:22 -0400

    Util fix

--- src/Utility/SouthPoleUtil.java
+++ src/Utility/SouthPoleUtil.java
@@ -5,8 +5,21 @@ package Utility;
 
 /**
  * @author NanflasTed
+ * @author WJLIDDY
  *
  */
 public class SouthPoleUtil {
+	
+	//CONVERT TO INT:Command.SIGNUP.ordinal();
+	//CONVERT TO ENUM:Command.values()[(int)Command.SIGNUP.ordinal()];
+	public static enum Command{
+		LOGIN,
+		SIGNUP,
+		GETCOND,
+		MOVEUP,
+		MOVELEFT,
+		MOVEDOWN,
+		MOVERIGHT
+	}
 	
 }
