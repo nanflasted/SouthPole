970351bc60981800bebae9de36a0b9dd1d1d4535
nanflasted <nanflasted@gmail.com>
2015-06-20 23:08:18 +0800

    Doge calculations. This one probably will not work
    @WJLiddy @jhoak when you get a chance let's discuss how economy should
    be set.

--- src/Server/Resource/DogeData.java
+++ src/Server/Resource/DogeData.java
@@ -10,7 +10,11 @@ public class DogeData implements Purchasable{
 	
 	public DogeData()
 	{
-		
+		food = Math.random()*10;
+		power = Math.random()*10;
+		failureProb = Math.random();
+		weight = Math.random() * 10;
+		price = ((int)power - (int)food)*3-weight;
 	}
 	
 	
